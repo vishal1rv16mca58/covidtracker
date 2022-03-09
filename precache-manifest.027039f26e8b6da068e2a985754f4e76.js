fd7fc9fe6b9253c5859ef49c3df54e6e2302ce71
VISHAL <vishal@VISHALs-MacBook-Air.local>
2022-03-09 14:34:21 +0530

    Updates

---
+++ precache-manifest.027039f26e8b6da068e2a985754f4e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38971bdeba839674edbf",
+    "url": "/covidtracker/static/css/main.c26f1d17.chunk.css"
+  },
+  {
+    "revision": "38971bdeba839674edbf",
+    "url": "/covidtracker/static/js/main.38971bde.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/covidtracker/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "f536cd67352e6b306ea6",
+    "url": "/covidtracker/static/js/2.f536cd67.chunk.js"
+  },
+  {
+    "revision": "32f9d6aa293b4cd8eac7",
+    "url": "/covidtracker/static/js/runtime~main.32f9d6aa.js"
+  },
+  {
+    "revision": "3cb946e94bb885449658b8bb3e335994",
+    "url": "/covidtracker/index.html"
+  }
+];
